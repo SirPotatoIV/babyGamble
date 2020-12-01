02460963ba6be6666e8babf6d30f5131e0e6ba0d
Jake O'Toole <otoolej33@gmail.com>
2020-11-30 21:13:50 -0600

    Created 3 components that will be the 3 pages for the site. Added a basic nav and set up the switch component for react router.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import About from './pages/About';
+import Guess from './pages/Guess';
+import Home from './pages/Home';
+
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Baby Gamble</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/guess">Guess</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/guess">
+            <Guess />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Guess.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Guess() {
+  return <h1>Guess</h1>;
+}
