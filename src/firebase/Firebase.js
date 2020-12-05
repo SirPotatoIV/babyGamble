44c9916c469cd2b598b23a8363a259192672930b
Jake O'Toole <otoolej33@gmail.com>
2020-12-05 13:56:41 -0600

    Added firebase config settings and used react env variables for all the important data. Passed the config to the initialize app method. Exported app. Still need to test if it actually works.

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,3 +1,15 @@
 import firebase from 'firebase';
 
-const app = firebase.initializeApp({});
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
