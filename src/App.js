d7d7a8d1277a3b21d4400aa5f019cb6f9b1b15a2
Jake O'Toole <otoolej33@gmail.com>
2020-12-04 19:25:27 -0600

    Updated React Routing setup to be slightly simpilier ... for now.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from './pages/About';
 import Guess from './pages/Guess';
 import Home from './pages/Home';
 
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 import './App.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        {/*  <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/guess">Guess</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav> */}
-        <Navbar />
-        <Switch>
-          <Route path="/guess">
-            <Guess />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/guess">
+          <Guess />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
