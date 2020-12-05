8c5ea7bf262c3fb81fcc41ccf6b7ef85f6b5d9d4
Jake O'Toole <otoolej33@gmail.com>
2020-12-04 21:22:13 -0600

    Added some basic inputs.

--- src/components/GuessForm/GuessForm.js
+++ src/components/GuessForm/GuessForm.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 
 export default function GuessForm() {
   return (
-    <>
-      <p>Test</p>
-    </>
+    <Box alignContent="center">
+      <form>
+        <div>
+          <TextField
+            id="firstName"
+            label="First Name"
+            type="text"
+            variant="outlined"
+          />
+          <TextField
+            id="lastName"
+            label="Last Name"
+            type="text"
+            variant="outlined"
+          />
+        </div>
+        <div>
+          <TextField
+            id="firstName"
+            label="First Name"
+            type="text"
+            variant="outlined"
+          />
+          <TextField
+            id="lastName"
+            label="Last Name"
+            type="text"
+            variant="outlined"
+          />
+        </div>
+      </form>
+    </Box>
   );
 }
