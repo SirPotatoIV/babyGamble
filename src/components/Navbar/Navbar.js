5d973ea0fb875d5e8bad229bac9deebda55f1323
Jake O'Toole <otoolej33@gmail.com>
2020-12-04 19:42:00 -0600

    Placed buttons for nav within a toolbar, which made the buttons become inline.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
 
 export default function NavBar() {
   return (
     <div>
       <AppBar position="static">
-        <Button component={Link} to="/guess" variant="text">
-          Guess
-        </Button>
-        <Button component={Link} to="/">
-          Home
-        </Button>
+        <Toolbar>
+          <Button component={Link} to="/">
+            Home
+          </Button>
+          <Button component={Link} to="/guess" variant="text">
+            Guess
+          </Button>
+          <Button component={Link} to="/about">
+            About
+          </Button>
+        </Toolbar>
       </AppBar>
     </div>
   );
