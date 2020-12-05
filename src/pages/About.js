72832079ff03d2186883895a32136b1e5ce38439
Jake O'Toole <otoolej33@gmail.com>
2020-12-04 20:37:16 -0600

    Updated Guess and About with the same styling updates as Home.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <Box color="text.primary">
+      <Typography variant="h1" align="center">
+        About
+      </Typography>
+    </Box>
+  );
 }
--- src/pages/Guess.js
+++ src/pages/Guess.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 export default function Guess() {
-  return <h1>Guess</h1>;
+  return (
+    <Box color="text.primary">
+      <Typography variant="h1" align="center">
+        Guess
+      </Typography>
+    </Box>
+  );
 }
