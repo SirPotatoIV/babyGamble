1e30e519d26fd70a7530b8e8dc6220b0f3fb7760
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 14:48:01 -0600

    Removed console log.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
   useEffect(() => {
     // Auth unsubscribe taken from Ben McMahen's article: https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
     const unsubscribeFromAuth = auth.onAuthStateChanged((newUser) => {
-      console.log('state changed');
       return setUser({
         initializing: false,
         user: newUser,
