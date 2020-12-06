4f96592ecb9e8015db4a8c6b8080b45c6ff640db
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 12:18:04 -0600

    Subscribing to user authentication status and added a method for unsubscribing so that it is not constantly running. Referenced article used to learn about how to setup with useEffect.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { firebase } from './components/Firebase';
+import React, { useEffect, useState } from 'react';
+import { auth } from './components/Firebase';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from './pages/About';
 import Guess from './pages/Guess';
@@ -10,6 +10,21 @@ import Navbar from './components/Navbar';
 import './App.scss';
 
 function App() {
+  const [user, setUser] = useState();
+  console.log(user);
+
+  useEffect(() => {
+    // Auth unsubscribe taken from Ben McMahen's article: https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
+    const unsubscribeFromAuth = auth.onAuthStateChanged(() =>
+      setUser({
+        initializing: false,
+        user,
+      })
+    );
+
+    return () => unsubscribeFromAuth();
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
