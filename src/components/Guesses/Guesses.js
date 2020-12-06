ebfab904d8b2f5fc116e6e9d29320aba17769319
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 13:45:30 -0600

    Copied code in Home being used to display guesses and pasted it as the base code for the component Guesses.

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Guesses = () => {
-  return <div>Guesses</div>;
+import Container from '@material-ui/core/Container';
+
+const Guesses = ({ guesses }) => {
+  return (
+    <Container>
+      {!!guesses &&
+        guesses.map((guess) => {
+          return (
+            <div key={guess.id}>
+              <div>{guess.email}</div>
+              <div>
+                {guess.firstName} {guess.lastName}
+              </div>
+              <div>{guess.hairColor}</div>
+            </div>
+          );
+        })}
+    </Container>
+  );
 };
 
 export default Guesses;
