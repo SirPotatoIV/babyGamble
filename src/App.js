ebae12a4c2fcfe1fd0e253a57eaad08b07eec1ac
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 18:27:37 -0600

    Removed code for getting auth state from App and any related imports.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { auth, createUserProfileDocument } from './components/Firebase';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from './pages/About';
 import Guess from './pages/Guess';
@@ -11,25 +10,6 @@ import Navbar from './components/Navbar';
 import './App.scss';
 
 function App() {
-  const [user, setUser] = useState();
-  console.log(user);
-
-  useEffect(() => {
-    // Auth unsubscribe taken from Ben McMahen's article: https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
-    const unsubscribeFromAuth = auth.onAuthStateChanged(
-      async (loggedInUser) => {
-        // If profile already exists, then it returns the user profile
-        const userProfile = createUserProfileDocument(loggedInUser.user);
-        return setUser({
-          initializing: false,
-          user: userProfile,
-        });
-      }
-    );
-    // Clean up for when we no longer need to listen to changes to auth
-    return () => unsubscribeFromAuth();
-  }, []);
-
   return (
     <BrowserRouter>
       <GuessesProvider>
