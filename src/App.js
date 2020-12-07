0f39649d105179648f82e9760f7d9dc4c4103e12
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 18:29:23 -0600

    Information about logged in user now provided to the entire application.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Guess from './pages/Guess';
 import Home from './pages/Home';
 
+import UserProvider from './providers/UserProvider';
 import GuessesProvider from './providers/GuessesProvider';
 import Navbar from './components/Navbar';
 
@@ -12,20 +13,22 @@ import './App.scss';
 function App() {
   return (
     <BrowserRouter>
-      <GuessesProvider>
-        <Navbar />
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/guess">
-            <Guess />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </GuessesProvider>
+      <UserProvider>
+        <GuessesProvider>
+          <Navbar />
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/guess">
+              <Guess />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </GuessesProvider>
+      </UserProvider>
     </BrowserRouter>
   );
 }
