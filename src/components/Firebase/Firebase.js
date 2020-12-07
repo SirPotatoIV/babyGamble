3cfd4fe23899be6850882e88a4e298cfb0b5b75c
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 19:19:25 -0600

    Corrected error in createUserDocument. the return statement that is meant to always happen was within an if statement block, which it should not have been.

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -26,14 +26,18 @@ const signInWithGoogle = () => auth.signInWithPopup(provider);
 const signOut = () => auth.signOut();
 
 const createUserProfileDocument = async (user, additionalData) => {
+  console.log('createUser', user);
   // if no user, exit
   if (!user) return;
   // Get reference to the user in the database, assuming they do exist
   const userRef = firestore.doc(`users/${user.uid}`);
+  console.log('userRef', userRef);
   // Go and fetch the document from the location.
   const snapshot = await userRef.get();
   // if a snapshot does not exist (meaning the user profile does not exist)
-  if (!snapshot.exist) {
+  console.log('snapshot', snapshot);
+  if (!snapshot.exists) {
+    console.log('Actually creating new user. snapshot must not exist');
     const createdAt = new Date();
     const { displayName, email } = user;
     try {
@@ -47,8 +51,9 @@ const createUserProfileDocument = async (user, additionalData) => {
       console.error('Error creating user', error);
     }
     // When a user profile has to be created, also get the user document and return it.
-    return getUserDocument(user.uid);
   }
+  console.log('just before calling getUserDocument');
+  return getUserDocument(user.uid);
 };
 
 const getUserDocument = async (uid) => {
