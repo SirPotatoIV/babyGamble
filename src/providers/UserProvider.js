010484c42b606b9a7b94f333113d5952ae48285d
Jake O'Toole <otoolej33@gmail.com>
2020-12-06 18:26:35 -0600

    Copy/pasted code for getting auth state from App into UserProvider.

--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -1,7 +1,28 @@
-import react, { useState, createContext } from 'React';
+import React, { useEffect, useState, createContext } from 'React';
+import { auth, createUserProfileDocument } from '../components/Firebase';
 
-const UserProvider = () => {
-  return <div></div>;
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    // Auth unsubscribe taken from Ben McMahen's article: https://dev.to/bmcmahen/using-firebase-with-react-hooks-21ap
+    const unsubscribeFromAuth = auth.onAuthStateChanged(
+      async (loggedInUser) => {
+        // If profile already exists, then it returns the user profile
+        const userProfile = createUserProfileDocument(loggedInUser.user);
+        return setUser({
+          initializing: false,
+          user: userProfile,
+        });
+      }
+    );
+    // Clean up for when we no longer need to listen to changes to auth
+    return () => unsubscribeFromAuth();
+  }, []);
+
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
 
 export default UserProvider;
