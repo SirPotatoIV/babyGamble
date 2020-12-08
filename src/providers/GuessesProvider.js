27205312e14d65da661dd0f6ef9d69edd2a903b1
Jake O'Toole <otoolej33@gmail.com>
2020-12-07 18:08:10 -0600

    Added error handling to the Guess Provider. An error will always occur at the start of the application, so if the error is insufficient permission, it should not be displayed.

--- src/providers/GuessesProvider.js
+++ src/providers/GuessesProvider.js
@@ -7,12 +7,19 @@ const GuessesProvider = ({ children }) => {
   const [guesses, setGuesses] = useState([]);
 
   useEffect(() => {
-    const unsubscribeFromGuesses = firestore
-      .collection('guesses')
-      .onSnapshot((snapshot) => {
+    const unsubscribeFromGuesses = firestore.collection('guesses').onSnapshot(
+      (snapshot) => {
         const userGuesses = snapshot.docs.map(collectIdsAndDocs);
         setGuesses(userGuesses);
-      });
+      },
+      (error) => {
+        // When the application initially starts, an error will occur because the user will not be logged in.
+        // An error for insufficient permissions should not be thrown to the console.
+        if (error.message !== 'Missing or insufficient permissions.') {
+          console.log(error.message);
+        }
+      }
+    );
     // clean up step for when we no longer need to be listening for updates to guesses
     return () => unsubscribeFromGuesses();
   }, []);
