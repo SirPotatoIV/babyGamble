3e1c43278f686d22395840ff263d4bb564a06307
Jake O'Toole <otoolej33@gmail.com>
2020-12-13 12:22:33 -0600

    Added styles to DatePicker.

--- src/components/GuessForm/DatePicker.js
+++ src/components/GuessForm/DatePicker.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: '120px',
+  },
+  textField: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const DatePicker = () => {
+  const classes = useStyles();
   <>
     <FormControl className={classes.formControl}>
       <InputLabel id="hairColorLabel">Hair Color</InputLabel>
