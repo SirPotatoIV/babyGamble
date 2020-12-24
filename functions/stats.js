d3b6198511b198f1fe82081ccceba1ed5dce4d2e
Jake O'Toole <otoolej33@gmail.com>
2020-12-24 14:43:52 -0600

    Created a new file to store all the functionality related to creating the stats/data that will be used to populate the graphs.

---
+++ functions/stats.js
@@ -0,0 +1,22 @@
+// bucketing
+// -- total number of guesses
+// -- calculate the range: max - min (there may be Math methods for this)
+// -- divide range by guesses
+
+// create variables to store each guess category and create a basic shape.
+// sum up the total count of sex, eyeColor, and hairColor by category
+// put all the weights into a single array
+// -- convert each weight guess into a single number
+// -- There are 16 ounces in a pound, and add that to the pounds property
+// put all the lengths into a single array
+// -- calculate how you are bucketing.
+// -- calculate totals in each bucket.
+// take the weight guess and the length guess and put then in an object as a pair
+// -- [{weight: 6.5, length: 22}, {weight: 8.6, length: 19}]
+// put all dates into a single array
+// -- convert into a date datatype
+// -- calculate how you are bucketing.
+// -- calculate totals in each bucket.
+// put all times into a single array.
+// -- calculate how you are bucketing.
+// -- calculate totals in each bucket.
