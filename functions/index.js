4362365e998f5e1cb871c0f9abaaf47ee7d10fe5
Jake O'Toole <otoolej33@gmail.com>
2020-12-23 20:18:55 -0600

    Created first cloud function.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// This initializes the app AND gets the secret keys needed to run admin functionality
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
+});
+
+exports.getAllGuesses = functions.https.onRequest(async (request, response) => {
+  const snapshot = await firestore.collection('guesses').get();
+  const guesses = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  response.json({ guesses });
 });
