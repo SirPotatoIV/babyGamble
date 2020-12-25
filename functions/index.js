e9f8aeb1957ef2de9c48f3575137d43b90885490
Jake O'Toole <otoolej33@gmail.com>
2020-12-25 17:13:32 -0600

    Commented out cloud functions, because they are not ready yet for deployment.

--- functions/index.js
+++ functions/index.js
@@ -13,29 +13,29 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
 
-exports.getAllGuesses = functions.https.onRequest(async (request, response) => {
-  const snapshot = await firestore.collection('guesses').get();
-  const guesses = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
+// exports.getAllGuesses = functions.https.onRequest(async (request, response) => {
+//   const snapshot = await firestore.collection('guesses').get();
+//   const guesses = snapshot.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   }));
 
-  response.json({ guesses });
-});
+//   response.json({ guesses });
+// });
 
-exports.updateStats = functions.firestore
-  .document('guesses/{guessId}')
-  .onWrite(async (change) => {
-    // prevents running the rest of the function if the onWrite was to delete the guess.
-    if (!change.after.exists) return;
+// exports.updateStats = functions.firestore
+//   .document('guesses/{guessId}')
+//   .onWrite(async (change) => {
+//     // prevents running the rest of the function if the onWrite was to delete the guess.
+//     if (!change.after.exists) return;
 
-    // addedToStats is used to indicate that this guess has been added to the total calculations.
-    // -- if we were using this function to update the guess itself, we could use addedToStats to prevent an infinite loop.
-    const { content, addedToStats } = change.after.data();
-    if(conent && !addedToStats) {
-      const 
-    }
+//     // addedToStats is used to indicate that this guess has been added to the total calculations.
+//     // -- if we were using this function to update the guess itself, we could use addedToStats to prevent an infinite loop.
+//     const { content, addedToStats } = change.after.data();
+//     if(conent && !addedToStats) {
+//       const
+//     }
 
-    if(content && !addedToStats)
+//     if(content && !addedToStats)
 
-  });
+//   });
